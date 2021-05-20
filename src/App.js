501978b64eaf3d4594a9baa8f83d1fb694a0df58
nicholastorr <nick.torres213@gmail.com>
2021-05-19 20:23:52 -0400

    added some info to home and image for test

--- src/App.js
+++ src/App.js
@@ -1,47 +1,45 @@
 import React, { Component } from "react"
-import logo from "./logo.svg"
+import headerImage from "./lambda/image.jpg";
 import "./App.css"
 
-class LambdaDemo extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { loading: false, msg: null }
-  }
 
-  handleClick = api => e => {
-    e.preventDefault()
+ 
+  
 
-    this.setState({ loading: true })
-    fetch("/.netlify/functions/" + api)
-      .then(response => response.json())
-      .then(json => this.setState({ loading: false, msg: json.msg }))
-  }
-
-  render() {
-    const { loading, msg } = this.state
-
-    return (
-      <p>
-        <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
-        <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
-        <br />
-        <span>{msg}</span>
-      </p>
-    )
-  }
-}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            <code>src/App.js</code> and save to reload.
-          </p>
-          <LambdaDemo />
-        </header>
+        
+        <div className="header">
+        <ul>
+            <li>Home</li>
+            <li>Web</li>
+            <li>Projects</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+
+
+
+        <div className="header-text">
+        <h2>Nicholas Esquilo Torres</h2>
+        <h4>Full Stack Web Developer</h4>
+        </div>
+
+        <img src={headerImage} alt="header"/>
+
+        <div className="header-intro">
+        <p>Hello! Thanks for checking out my portfolio.</p>
+        <p>I am web developer based out of Tampa, Florida and have a passion for computers</p>
+        </div>
+
+
+
+        </div>
+
+       
       </div>
     )
   }
